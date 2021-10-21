576e18d90cccec986d7897f99343db6053d28c43
JosiahMg <JosiahMg@163.com>
2021-10-21 18:30:34 +0800

    move config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # 项目根目录
-root_dir = os.path.dirname(os.path.dirname(__file__))
+root_dir = os.path.dirname(__file__)
 # 项目语料原始数据目录
 root_corpus_dir = os.path.join(root_dir, 'corpus/origin')
 # 处理后数据存放路径
--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from collections import Counter
-import nltk
-
-text = """Our legislation on participating in a bid is clear: no one can be prohibited, 
-          said Mourao, adding the only thing the company must do is to demonstrate its 
-          transparency (in keeping) with the rules that will be established for the process."""
+import config
 
 
-text = nltk.word_tokenize(text)
-
-ct = Counter()
-
-
-for word in text:
-    ct[word] += 1
-
-print(ct)
-print(list(ct.elements()))
+print(config.root_corpus_dir)
