ba2dae04f75326f10f8e2b5e7d2ea776443c2bab
JosiahMg <JosiahMg@163.com>
2021-10-21 17:21:10 +0800

    corpus_stream.py

--- day_exercise.py
+++ day_exercise.py
@@ -1,44 +1,67 @@
+"""
+每天随机选择一个程序进行练习
+"""
+
 import os
 import json
 import numpy as np
 from pprint import pprint
 from typing import List, Text
 
-
+# 保存复习进度的文件
 candidate_file = 'candidates.json'
+# 需要复习的文件夹
 base_dirs = ['data-processing', 'models', 'python-package']
-ignore_file = ['config.py', 'main.py']
+# 忽略不需要复习的文件
+ignore_file = ['config.py', 'counter.py']
+# 需要复习的次数
+exercise_times = 100
 
-# 初始化每个文件的权重
+
 def init_candidate(all_files):
+    """
+    初始化每个文件的权重
+    foramt:
+    {
+        filename: {
+            "probability": float,
+            "counter": int
+        }
+    }
+    """
     num = len(all_files)
     value = 1.0/num
-    return {file: value for file in all_files}
+    return {file: {'probability': value, 'counter': 0} for file in all_files}
 
 
 # 加载候选名单,files为当前总文件列表
 def load_candidate(all_files: List[Text]):
     new_lists = []
-    new_candidates = {}
     if not os.path.exists(candidate_file):
-        new_candidates = init_candidate(all_files)
+        candidates = init_candidate(all_files)
     else:
         candidates = json.load(open(candidate_file, 'r'))
         # 首先遍历词典，找到已经不存在的文件的key并删掉
         discard_weight = 0.0
+        discard_files = []
         for key, value in candidates.items():
             if key not in all_files:
-                discard_weight += value
+                discard_weight += value['probability']
+                discard_files.append(key)
         each_discard_weight = discard_weight/len(all_files)
-        # 遍历所有文件，看看是否有新增加的文件
 
+        # 删除不存在的文件的key
+        for file in discard_files:
+            del candidates[file]
+
+        # 遍历所有文件，看看是否有新增加的文件
         for file in all_files:
             if file not in candidates:
-                new_candidates[file] = each_discard_weight
+                candidates[file] = {'probability': each_discard_weight, 'counter': 1}
                 new_lists.append(file)
             else:
-                new_candidates[file] = candidates[file] + each_discard_weight
-    return new_lists, new_candidates
+                candidates[file]['probability'] = candidates[file]['probability'] + each_discard_weight
+    return new_lists, candidates
 
 
 # 保存pkl文件
@@ -51,13 +74,16 @@ def update_candidate_weights(chosen_files, all_files, candidates):
     weights = 0.0
     num_files = len(all_files) - len(chosen_files)
     for file in chosen_files:
-        weights += candidates[file]
-        candidates[file] = 0
+        weights += candidates[file]['probability']
+        candidates[file]['probability'] = 0.0
+        if candidates[file]['counter'] != exercise_times:
+            candidates[file]['counter'] += 1
 
     each_weight = weights/num_files
     for key, _ in candidates.items():
         if key not in chosen_files:
-            candidates[key] += each_weight
+            candidates[key]['probability'] += each_weight
+    return candidates
 
 
 def get_all_files():
@@ -70,12 +96,14 @@ def get_all_files():
     return candidate_files
 
 
+# 统计所有文件的概率值是否为1
 def candidate_weight_sum():
     if not os.path.exists(candidate_file):
         pprint('Candidate json file not exists')
     else:
         candidates = json.load(open(candidate_file, 'r'))
-        print(np.sum(list(candidates.values())))
+        print(np.sum([weight['probability'] for weight in list(candidates.values())]))
+
 
 # 测试使用打印信息
 def show_candidate_info():
@@ -84,8 +112,27 @@ def show_candidate_info():
     else:
         candidates = json.load(open(candidate_file, 'r'))
         infos = zip(list(candidates.keys()), list(candidates.values()))
-        infos = sorted(infos, key=lambda x: -x[1])
+        infos = sorted(infos, key=lambda x: -x[1]['counter'])
         pprint(infos)
+
+
+def softmax(x: List[int]):
+    y = np.exp(x-np.max(x))
+    fx = y / np.sum(y)
+    return fx
+
+
+# 获取random.choice的概率分布
+def get_choice_probability(values):
+    probas = []
+    counters = []
+    for value in values:
+        probas.append(value['probability'])
+        counters.append(exercise_times - value['counter'])
+
+    counters = softmax(counters)
+    result = [(pro/2.+counter/2.) for pro, counter in zip(probas, counters)]
+    return result
 
 
 def main_process():
@@ -100,10 +147,13 @@ def main_process():
         for key, value in candidates.items():
             files.append(key)
             weights.append(value)
+        weights = get_choice_probability(weights)
         choice_file.append(np.random.choice(files, p=weights))
-        update_candidate_weights(choice_file, all_files, candidates)
+
+        candidates = update_candidate_weights(choice_file, all_files, candidates)
+
     save_load_candidate(candidates)
-    return choice_file
+    return [(file, candidates[file]['counter']) for file in choice_file]
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from smart_open import open
 
 
-d2 = ['a', 'b', 'c']
+class IterRead:
+    def __iter__(self):
+        for line in open('https://radimrehurek.com/gensim_3.8.3/auto_examples/core/mycorpus.txt'):
+            yield line.strip()
 
-print(list(zip(d1, d2)))
+
+iter_data = IterRead()
+
+for line in iter_data:
+    print(line)
