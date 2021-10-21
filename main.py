c401c0188a9b3a77b8b887a94bb5498841e547b2
JosiahMg <JosiahMg@163.com>
2021-10-21 09:12:16 +0800

    优化小黄鸡语料处理程序

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-import config
-import os
-import numpy as np
 import pandas as pd
-from time import time
+from pprint import pprint
 
-
-t1 = time()
-
-def func1():
-    with open('corpus/origin/estate/train.reply.tsv', encoding='utf-8') as f:
-        while True:
-            yield f.readline()
-
-d1 = func1()
-for d in d1:
-    print(d)
-print(time()-t1)
-
-
-t1 = time()
-
-def func2():
-    for line in open('corpus/origin/estate/train.reply.tsv', encoding='utf-8'):
-        yield line
-
-d2 = func2()
-
-print(time()-t1)
-
+data = '        hello        '
+pprint(data.strip())
--- xiaohuangji_csv.py
+++ xiaohuangji_csv.py
@@ -26,20 +26,67 @@ import pandas as pd
 from tqdm import tqdm
 
 
-with open(config.xiaohuangji_corpus, encoding='utf-8') as f:
-    flag = 0
-    df = pd.DataFrame(columns=['question', 'answer'])
-    df_dict = {}
-    for line in tqdm(f.readlines()):
-        if line.startswith('E'):
-            continue
-        elif line.startswith('M') and flag == 0:
-            df_dict['question'] = line[2:].strip()
-            flag = 1
-        else:
-            df_dict['answer'] = line[2:].strip()
-            df = df.append([df_dict])
-            flag = 0
+def create2csv_all_in_one():
+    """
+    时候文件较小时，一次性读入到内存最后写入文件
+    :return:
+    """
+    with open(config.xiaohuangji_corpus, encoding='utf-8') as f:
+        flag = 0
+        df = pd.DataFrame(columns=['question', 'answer'])
+        df_dict = {}
+        for line in tqdm(f.readlines()):
+            if line.startswith('E'):
+                continue
+            elif line.startswith('M') and flag == 0:
+                df_dict['question'] = line[2:].strip()
+                flag = 1
+            else:
+                df_dict['answer'] = line[2:].strip()
+                # 如果question和answer都存在时则保存
+                if df_dict['question'] and df_dict['answer']:
+                    df = df.append([df_dict])
+                flag = 0
 
-# index=False必须指定，否则生成的文件最左侧会出现index
-df.to_csv(config.xiaohuangji_csv, encoding='utf-8', sep=',', index=False)
+    # index=False必须指定，否则生成的文件最左侧会出现index
+    df.to_csv(config.xiaohuangji_csv, encoding='utf-8', sep=',', index=False)
+
+
+def create2csv_block(block=1000):
+    """
+    当文件较大时，没读入block数据就追加到csv文件中,这样占用内存较小，速度会快很多
+    :param block: 每次读入多少行数据
+    :return:
+    """
+    with open(config.xiaohuangji_corpus, encoding='utf-8') as f:
+        flag = 0
+        count = 0
+        df = pd.DataFrame(columns=['question', 'answer'])
+        df_dict = {}
+        for line in tqdm(f.readlines()):
+            if line.startswith('E'):
+                continue
+            elif line.startswith('M') and flag == 0:
+                df_dict['question'] = line[2:].strip()
+                flag = 1
+            else:
+                df_dict['answer'] = line[2:].strip()
+                # 如果question和answer都存在时则保存
+                if df_dict['question'] and df_dict['answer']:
+                    if count % block == 0:
+                        # mode='a': 追加模式, mode='w':覆盖模式
+                        # header=False: 不添加coulums
+                        df.to_csv(config.xiaohuangji_csv, mode=('a' if count else 'w'),
+                                  encoding='utf-8', sep=',', index=False,
+                                  header=(False if count else True))
+                        df.drop(df.index, inplace=True)
+                    df = df.append([df_dict])
+                    count += 1
+                flag = 0
+        # 最终的数据也需要写入
+        df.to_csv(config.xiaohuangji_csv, encoding='utf-8',  mode='a', sep=',', index=False, header=False)
+
+
+if __name__ == '__main__':
+    create2csv_block()
+
